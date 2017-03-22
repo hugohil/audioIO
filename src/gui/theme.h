18b2633beae78b0a0798c062edf50174bf7de186
hugohil <hilairehugo@gmail.com>
2017-03-22 19:49:35 +0100

    Fix gui images path

--- src/gui/theme.h
+++ src/gui/theme.h
@@ -7,11 +7,11 @@ public:
 
     ofxDatGuiThemeFUBAR()
     {
-        icon.radioOnPath = "gui/images/picker-rainbow.png";
-        icon.radioOffPath = "gui/images/icon-radio-on.png";
-        icon.groupOpenPath = "gui/images/icon-radio-off.png";
-        icon.groupClosedPath = "gui/images/icon-group-open.png";
-        icon.rainbowPath = "gui/images/icon-group-closed.png";
+        icon.radioOnPath = "gui/images/icon-radio-on.png";
+        icon.radioOffPath = "gui/images/icon-radio-off.png";
+        icon.groupOpenPath = "gui/images/icon-group-open.png";
+        icon.groupClosedPath = "gui/images/icon-group-closed.png";
+        icon.rainbowPath = "gui/images/picker-rainbow.png";
         font.file = "gui/fonts/Verdana.ttf";
 
         init();
