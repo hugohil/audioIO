c5ed903ff6832e6b848b98117fc2dcaa720560bb
hugohil <hilairehugo@gmail.com>
2018-01-26 14:05:11 +0100

    Update autorun script

--- autorun.sh
+++ autorun.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+###
+# to make this script executable via cmd+O or double click,
+# simply rename it from `autorun.sh` to `autorun.command`.
+# you might also need to change the path to `audioIO.app` in $COMMAND
+##
+
 export DISPLAY=:0.0
 
 PROGRAM='audioIO.app'
-COMMAND='open -n ./bin/audioIO.app --args --host 127.0.0.1 --device 4 --autostart'
+COMMAND='open -n ./bin/audioIO.app --autostart'
 
 while true; do
   if ps aux | grep -v grep | grep $PROGRAM
